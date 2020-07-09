3c7d91a4a162cb78f07fccab0873915c0a798fd2
VincentVillacorta <44991867+VincentVillacorta@users.noreply.github.com>
2020-07-08 22:58:02 -0700

    Added mysql database (infinityfree) to count

--- index.js
+++ index.js
@@ -20,11 +20,37 @@ const client = new tmi.client(options);
 client.connect();
 
 client.on('connected',(address,port) => {
-    client.action('CoachBot_1', 'CoachBot has arrived.')
+    client.action('CoachBot_1', 'CoachBot has arrived.');
 });
 
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'sql308.epizy.com',
+  user: 'epiz_26164688',
+  password: 'yctLPhSWoB8x',
+  database: 'epiz_26164688_CoachBotDB'
+});
+
+connection.connect((err) => {
+  if (err) console.error('error connecting: ', err);
+  console.log('Connected!');
+});
+
+
 client.on('chat', (channel,user,message,self) => {
-    if(message === '!burpee'){
-        client.action('CoachBot_1', `${client.identity.username} owes the chat 5 burpees.`);
+    if(message === '!burpees'){
+        connection.query(mysql, function (err, result) {
+        var sql = "INSERT INTO burpees (count) VALUES 2";
+            if (err) throw err;
+            console.log("One burpee added");
+        });
+        var count = connection.query("SELECT count FROM burpees", function (err, result, fields) {
+            if (err) throw err;
+            return result.count;
+          });
+
+        client.action('CoachBot_1', `${options.identity.username} owes the chat ${count} burpees.`);
     }
 });
