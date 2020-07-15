d45100f6e0eff5adb719254c1877f574e0f64d02
VincentVillacorta <44991867+VincentVillacorta@users.noreply.github.com>
2020-07-14 19:16:56 -0700

    *FUNCTIONAL WITH TWEAKS - handle duplicate rows - display instructions on connection

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*const tmi = require('tmi.js');
+const tmi = require('tmi.js');
 
 const options = {
     options: {
@@ -23,40 +23,108 @@ client.on('connected',(address,port) => {
     client.action('CoachBot_1', 'CoachBot has arrived.');
 });
 
-*/
+
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   host: 'database-coachbot.cogy1so7fjll.us-east-2.rds.amazonaws.com',
   port: '3306',
   user: 'admin',
-  password: 'Megaman2498!'
+  password: 'Megaman2498!',
+  database: 'coachbotDB'
 });
 
 connection.connect((err) => {
-  if (err) console.error('error connecting: ', err);
-  console.log('Connected!');
-  connection.query(mysql, function (err, result) {
-    var sql = "CREATE TABLE workouts (id INT AUTO_INCREMENT PRIMARY KEY, burpees VARINT(0), pushups(0), squats(0))";
+    if (err) console.error('error connecting: ', err);
+    console.log('Connected!');
+    connection.query("CREATE TABLE IF NOT EXISTS workouts (pushups INT(4), burpees INT(4), squats INT(4))", function (err, result) {
         if (err) throw err;
         console.log("Workouts Table Created.");
-    });
-        
+        });
+    connection.query("INSERT IGNORE INTO workouts (pushups, burpees, squats) VALUES ('0','0','0')", function (err, result) {
+        if (err) throw err;
+        console.log("Workouts set to 0.");
+        });
 });
 
-/*
+
 client.on('chat', (channel,user,message,self) => {
-    if(message === '!burpees'){
-        connection.query(mysql, function (err, result) {
-        var sql = "INSERT INTO burpees (count) VALUES (2)";
+    if(message === '!burpee'){
+        connection.query("UPDATE workouts SET burpees = burpees + 1", function (err, result) {
             if (err) throw err;
             console.log("One burpee added");
         });
-        var count = connection.query("SELECT count FROM burpees", function (err, result, fields) {
+        connection.query("SELECT burpees FROM workouts", function (err, result, fields) {
             if (err) throw err;
+            console.log(result);
+            client.action('CoachBot_1', `${options.channels[0]} owes the chat ${result[0].burpees} burpees.`);
+            return result;
+          });
+    }
+});
+
+client.on('chat', (channel,user,message,self) => {
+    if(message === '!pushup'){
+        connection.query("UPDATE workouts SET pushups = pushups + 1", function (err, result) {
+            if (err) throw err;
+            console.log("One pushup added");
+        });
+        connection.query("SELECT pushups FROM workouts", function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+            client.action('CoachBot_1', `${options.channels[0]} owes the chat ${result[0].pushups} pushups.`);
+            return result;
+          });
+    }
+});
+
+client.on('chat', (channel,user,message,self) => {
+    if(message === '!squat'){
+        connection.query("UPDATE workouts SET squats = squats + 1", function (err, result) {
+            if (err) throw err;
+            console.log("One squat added");
+        });
+        connection.query("SELECT squats FROM workouts", function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+            client.action('CoachBot_1', `${options.channels[0]} owes the chat ${result[0].squats} squats.`);
+            return result;
+          });
+    }
+});
+
+client.on('chat', (channel,user,message,self) => {
+    if(message === '!runit'){
+        connection.query("SELECT burpees FROM workouts", function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+            client.action('CoachBot_1', `${options.channels[0]} will now do ${result[0].burpees} burpees.`);
+            return result;
+          });
+        connection.query("SELECT pushups FROM workouts", function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+            client.action('CoachBot_1', `${options.channels[0]} will now do ${result[0].pushups} pushups.`);
+            return result;
+          });
+        connection.query("SELECT squats FROM workouts", function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+            client.action('CoachBot_1', `${options.channels[0]} will now do ${result[0].squats} squats.`);
             return result;
           });
 
-        client.action('CoachBot_1', `${options.identity.username} owes the chat ${count} burpees.`);
+        client.action('CoachBot_1', `All workouts have been reset.`);
+
+        connection.query("UPDATE workouts SET burpees = 0", function (err, result) {
+            if (err) throw err;
+        });
+        connection.query("UPDATE workouts SET pushups = 0", function (err, result) {
+            if (err) throw err;
+        });
+        connection.query("UPDATE workouts SET squats = 0", function (err, result) {
+            if (err) throw err;
+        });
+
     }
-});*/
+});
